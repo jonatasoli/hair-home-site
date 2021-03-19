d21fa84f80657680e2d00d4d99177dbed85cf1ac
Thais Martins <thaismartins1999@gmail.com>
2021-03-19 12:27:29 -0300

    add phone

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     color="#18121E" padless height="200" width="auto" class="mt-auto">
   <v-card color="#18121E" elevation="0" class="mx-auto lighten-6 py-2 text-center">
        <p>Academia Brasileira de Beleza Hair School - CNPJ: 09.675.354/0001-00 © Todos os direitos reservados. 2020</p>
-       <p>relacionamento@graciellegatto.com.br</p> <a href="/politicas-de-privacidade" style="color: white" >POLÍTICAS DE PRIVACIDADE</a>
+       <p>relacionamento@graciellegatto.com.br | +55 (11) 99386-0382</p> <a href="/politicas-de-privacidade" style="color: white" >POLÍTICAS DE PRIVACIDADE</a>
          <a href="/termos-de-uso" style="color: white" >TERMOS DE USO</a>
        <!-- <v-spacer></v-spacer>
       <v-btn text >Termos de uso</v-btn>
