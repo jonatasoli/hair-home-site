1a50747064ee610da7ba1138f89f5dbb6e439c1e
Thais Martins <thaismartins1999@gmail.com>
2021-04-05 11:03:18 -0300

    refactor url

--- src/services/service.js
+++ src/services/service.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-    baseURL: process.env.VUE_APP_BASE_URL,
+    baseURL: process.env.VUE_BASE_URL,
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
